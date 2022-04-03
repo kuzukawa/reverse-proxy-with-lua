87dc4305dc47e7bc6aed96c349008666d0a1582f
Keisuke Kuzukawa <kuzukawa@gmail.com>
2022-04-03 19:12:28 +0900

    refactoring. move lua logic to .lua

--- containers/nginx/lua/proxy.lua
+++ containers/nginx/lua/proxy.lua
@@ -4,4 +4,11 @@ function _M.greet(name)
     ngx.say("Greetings from ", name)
 end
 
+function _M.rewrite_it_to_lua(body)
+    if body then
+        body = ngx.re.gsub(body, "It", "Lua")
+    end
+    return body
+end
+
 return _M
